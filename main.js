1d382de4b337354c86198560945026caaf4703a2
Ubuntu <ubuntu@ip-172-31-36-17.ap-southeast-1.compute.internal>
2018-03-05 04:29:46 +0000

    add RPC and HTTP access

--- main.js
+++ main.js
@@ -6,26 +6,27 @@ const requestx = require('request')                                             
 const bodyParser = require('body-parser')
 
         /* variable */
-burrowURL = "http://localhost:1337/rpc"; //using json
+burrowURL = "http://0.0.0.0:1337"; //using json
+burrowrpcURL = "http://localhost:1337/rpc"
 keysURL = "http://localhost:4767";
-accountPath = "/.monax/chains/multichain/account.json";
+//accountPath = "/.monax/chains/multichain/account.json";
 contractPath = "./smart.bin";
 contractABI = "./smart.abi";
 
-account = fs.readJSONSync(accountPath);
-binary = fs.readFileSync(contractPath);
+// account = fs.readJSONSync(accountPath);
+// binary = fs.readFileSync(contractPath);
 // ABI = fs.readJSONSync(contractABI);
 
 /* traditional way */
-address = require('./epm.output.json').deployStorageK;
-ABI = JSON.parse(fs.readFileSync('./abi/' + address, 'utf8'));
+// address = require('./epm.output.json').deployStorageK;
+// ABI = JSON.parse(fs.readFileSync('./abi/' + address, 'utf8'));
 
 // bind burrow, account, contract
-var contractManager = monax.newContractManagerDev(burrowURL, account.multichain_full_000);
+// var contractManager = monax.newContractManagerDev(burrowURL, account.multichain_full_000);
 
 // create factory for contract with JSON interface(abi)
 // .at(address) old version| compiled smart contract
-var contractFactory = contractManager.newContractFactory(ABI).at(address);
+// var contractFactory = contractManager.newContractFactory(ABI).at(address);
 
 // create new instance, deploy a contract use `new`:
 // var mycontract;
@@ -79,15 +80,17 @@ req.query.shoe.type
 
 app.get('/end', (request, response) => {
         // only use req.query for convenient dev
-        let method = "burrow." + request.query.method ;
+        let method = 'burrow.' + request.query.method ;
         console.log("request method >> " + method);
+	let address = request.query.addr;	
+
         let jsonDataObj = {
-                "jsonrpc": "2.0",
-                "method": method;
+                'jsonrpc': '2.0',
+                'method': method
         }                    
         var options = {
                 headers: {'content-type' : 'application/json'},
-                url  :  rpcURL,
+                url  :  burrowURL,
                 body :  jsonDataObj,
                 json :  true
         };
@@ -95,9 +98,8 @@ app.get('/end', (request, response) => {
         let _error;
         let _response;
         let _body;
-    
         
-        //console.log(options);
+        console.log(options);
         /* Requestx */
         var reqq = requestx.post(options, (error, res, body) => {
             console.log('error:', error);
@@ -119,7 +121,39 @@ app.get('/end', (request, response) => {
         /* end requestx*/
 });
 
+app.get('/web', (request, response) => {
+        let method = request.query.method ;
+        console.log("request web method >> " + method);
 
+        var options = {
+                url  :  burrowURL + '/' + method 
+        };
+
+        let _error;
+        let _response;
+        let _body;
+
+        console.log(options);
+        /* Requestx */
+        var reqq = requestx.get(options, (error, res, body) => {
+            console.log('error:', error);
+            console.log('statusCode:', res && res.statusCode);
+            console.log('body:', body);
+
+            _error = error;
+            _response = res;
+            _body = body;
+
+            /* send body with RAW format (chrome extension) */
+            if (!error && res.statusCode == 200) {
+                response.send(JSON.parse(body));
+            }else{
+                console.log("request error", error);
+                response.send(error);
+            }
+        });
+        /* end requestx*/
+});
 
 app.listen(port, (err) => {
         if (err) {
