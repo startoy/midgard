d0371b0aa26c0b01c80b1a30c1533819817eb31b
LaziEgg <peerapat.kk@hotmail.com>
2018-03-12 14:20:28 +0700

    add Test case

--- testContract.sol
+++ testContract.sol
@@ -1,16 +1,17 @@
+pragma solidity ^ 0.4.4;
 contract TestContract {
 
     address public testAddress;
 
     event AddressSet(boolean indexed itWasSet);
 
-    function getInts() constant returns (int a, int b){
+    function getInts() constant returns (int a, int b) {
         a = -10;
         b = -50;
         AddressSet(false);
     }
 
-    function getUints() constant returns (uint a, uint b){
+    function getUints() constant returns (uint a, uint b) {
         a = 10;
         b = 50;
         AddressSet(false);
