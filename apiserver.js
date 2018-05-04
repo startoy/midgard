eb276bb9ec86c4fef00bbe772e253770eeb32eaa
startoy <peerapat.skt@hotmail.com>
2018-05-04 09:10:28 +0000

    detail update, add more api, call test contract pass

--- apiserver.js
+++ apiserver.js
@@ -4,7 +4,10 @@ const contracts         = require('@monax/legacy-contracts');
 const burrowModule      = require('@monax/legacy-db');
 const express           = require('express')                          /* server provider, easy way to create node server with url handle */
 const request           = require('request')                          /* make a http request to specific url */
-const bodyParser        = require('body-parser')
+const bodyParser        = require('body-parser');
+
+/* import module from 'module'; */
+const util 		= require('./module');
 
         /* variable */
 _burrowURL        	= "http://0.0.0.0:1337";
@@ -20,8 +23,7 @@ var accountData         = require('/home/ubuntu/.monax/chains/multichain/account
 // var contractManager     = contracts.newContractManagerDev(burrowrpcURL, accountData.multichain_full_000); 
 
         /* FULL (newContractManagerDev)  */
-var burrow 		= burrowModule.createInstance(_burrowrpcURL);                           /* legacy-db */
-var tx                  = burrow.txs();                                                         /* transaction burrow */                                   
+var burrow 		= burrowModule.createInstance(_burrowrpcURL);      /* legacy-db */
 var pipe 		= new contracts.pipes.DevPipe(burrow, accountData.multichain_full_000); /* legacy-contract */
 var contractManager 	= contracts.newContractManager(pipe);
 var myContract          = contractManager.newContractFactory(ABI).at(address);
@@ -32,25 +34,25 @@ const port      	= process.env.PORT || 8080;
 const _url              = "http://0.0.0.0:" + port;
 const _url_acc          = _url + "/acc";
 const _url_tx           = _url + "/tx";
+const _url_sol		= _url + "/sol";
 
         /* for .post request */
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-        extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var account = express.Router();
 var tx      = express.Router();
+var sol	    = express.Router();
 
         /****************************************************************
          ******** ACCOUNT 
          *****************************************************************/
-
+	/*
         account.use( (req, res, next) => {
                 console.log("Entering the account acc -> next()");
                 next();
         });
-
+	*/
         account.get('/', (req, res) => {
                 res.json({ message : "this is first page of api"});
         });
@@ -74,16 +76,16 @@ var tx      = express.Router();
                         let options = { url : _url_acc + '/generate' };
                         request.get(options, (err, res2, body) => {
                                 if(!err && res2.statusCode == 200) {
-                                        if(addAccount(res2)){
+                                        if(pipe.addAccount(JSON.parse(body))){
                                                 console.log("success add account");
-                                                res.json({ message : "success", status : 1});
+                                                res.json(util.resLog("success", 1, JSON.parse(body)));
                                         }else{
                                                 console.log("failed add account");
-                                                res.json({ message : "failed", status : 0});
+                                                res.json(util.resLog("failed", 0));
                                         }
                                 }else{
                                         console.log("error on request to generate account");
-                                        res.json( { message : "error on request to generate account", status : 0} );
+                                        res.json( util.resLog("error on request to generate account",0) );
                                 }
                         });
                 });
@@ -101,7 +103,7 @@ var tx      = express.Router();
                                 res.json(newDataObj);
                             }else{
                                 console.log(error);
-                                res.json({ error : "something went wrong on generate account"});
+                                res.json( util.resLog("something went wrong on generate account", 0));
                             }
                         });
                 });
@@ -111,46 +113,72 @@ var tx      = express.Router();
          ******** TRANSACTION 
          *****************************************************************/
 
+	/*
         tx.use( (req, res, next) => {
                 console.log("Entering the transaction acc -> next()");
                 next();
         });
-
+	*/
         tx.get('/', (req, res) => {
-                res.json({ message : "this is first page of transaction api"});
+                res.json(util.resLog("this is first page of transaction api",1));
         });
 
         /*      /tx/sendtoken                   POST    send token by address with privkey and amount
         */
         tx.route('/sendtoken')
                 .get( (req, res) => {
-                        res.json({ message : "use POST instead."});
+			res.json(util.resLog("use POST method to send token",0));	
                 })
                 .post((req, res) => {
-                        /* FIXME: no indicator if is error or not */
-                        tx.sendAndHold( req.body.privKey, req.body.address, req.body.amt, globalCallback() );
-                        res.json({ message : "the transaction may be in queue for validate, you can quit this page for now"});
-                })
 
+                        /* FIXME: no indicator if is error or not */
+			let tx     = burrow.txs(); 
+                        tx.sendAndHold( req.body.privkey, 
+					req.body.address, 
+					req.body.amount,
+					(i)=>{console.log("called" + i); }
+				      );
+                        //res.json( { 	message : "this call may take time, you can quit this page for now", status : 1 });
+			res.json(util.resLog("this call may take time, you can quit this page for now", 1 ));
+                });
 
         /****************************************************************
-         ******** UTILITY 
+         ******** SMART CONTRACT / SOLIDITY
          *****************************************************************/
 
-        function addAccount(jsonObj){
-                // let newAccount = { address : addr, pubKey : pubk, privKey : prik };
-                return pipe.addAccount(jsonObj);
-        }
+	sol.get('/', (req, res) => {
+		res.json(util.resLog("this is first page of smart contract api",1));
+	});
 
-        function globalCallback(){
-                console.log("Operated...");
-        }
+	sol.route('/stock')
+		.get((req, res) => {
+			res.json(util.resLog("use POST instead"));
+		})
+		.post((req, res) => {
+			let result;
+			myContract.add_stock( 	req.body.id,
+					  	req.body.name,
+						req.body.amount,
+						req.body.price,
+						{from : req.body.address},
+						(error, res2) => {
+						// depend on smart contract if is return something.
+						if(!error){ result = res2; }else{ result = error;}
+						res.json(util.resLog(result));
+					})
+		});
 
-/* All account request prefix with "/acc" */
-app.use('/acc', account);
-/* All transaction request prefix with "/tx" */
-app.use('/tx', tx);
+	/* TODO: add more api
+	   	 use real smart contract
+		TODO:
+			CLIENT SCREEN CALL  */
 
+
+
+
+app.use('/acc', account);		/* All account request prefix with "/acc" */
+app.use('/tx', tx);			/* All transaction request prefix with "/tx" */
+app.use('/sol', sol);			/* All smart contract/solidity job prefix with "/sol" */
 app.listen(port, (err) => {
         if (err) {
                 return console.log('Fail to intial server:', err);
--- module.js
+++ module.js
@@ -1,9 +1,35 @@
-/* if have to use Module */
+        /****************************************************************
+         ******** UTILITY
+         *****************************************************************/
 
-export function hello() {
-  return "Hello";
-}
+        /* if support JS ES6
+	export function addAccount(jsonObj){
+                return pipe.addAccount(jsonObj);
+        }*/
+	
+	// commonJS way
+	exports.addAccount = (jsonObj) => {
+		 return pipe.addAccount(jsonObj);
+	}
 
-export function convert() {
-  return "convert";
-}
+        /* Response like LOG as JSON format */
+        /* msg 	  = string
+           status = int  [ -1, 0 = FALSE, ERROR, 1 = TRUE, SUCCESS, DEFAULT ]
+	   *strict msg and status have to return
+        */
+
+	exports.resLog = (msg, status, body) => {
+		//callback && callback();   // if have callback, call that
+		if(!msg){ msg = "" };
+		if(!status){ status = 1; }
+		return { message : msg, status : status, body };
+	}
+
+        exports.resLogCallBack = (msg, status, body, callback) => {
+                callback && callback();   // if have callback, call that
+		if(!msg){ msg = "" };
+		if(!status){ status = 1; }
+                return { message : msg, status : status, body };
+        }
+
+
